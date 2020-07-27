d05f10ed32442034b2e68db49cbb013f93914aee
unknown <dromerovarela776@gmail.com>
2020-07-27 12:52:14 -0500

    ultimos cambios

--- src/validator.js
+++ src/validator.js
@@ -7,26 +7,23 @@ const validator = {
     let duplicateNumbers = [];
     let finalArry = undefined;
     let sum = 0;
-    // let validCard = false;
+    
 
     //  A traves del método split dividimos el numero de la  tarjeta(que es un string) en una matriz de cadenas
     //  mediante la separación de la cadena en sub cadenas
 
     let cardNumberSplit = cardNumber.split("").reverse();
-    // console.log(cardNumber);
-    // console.log(cardNumberSplit);
+    
 
-
-    // if (cardNumber.length >=13 ) {
         // Creamos un for para poder iterar con cada una de las sub cadenas del array
         for ( let i = 0; i < cardNumberSplit.length; i++) {
             if (i % 2 == 0) {
-                // console.log(i);
+                
                 individualNumbers.push(cardNumberSplit[i]);// Usamos método push() para los elementos que cumplen con la condición  al final de un nuevo arreglo
                 
             } else {
-                duplicateNumbers.push((cardNumberSplit[i] * 2).toString());
-                // console.log("numeros posicion2 multiplicados" + duplicateNumbers)
+                duplicateNumbers.push((cardNumberSplit[i] * 2));
+                console.log("numeros posicion2 multiplicados" + duplicateNumbers)
             }
         }
 
@@ -35,15 +32,14 @@ const validator = {
     //De esta manera cada numero es un solo dígito y se convierte en array otra vez
 
     duplicateNumbers = duplicateNumbers.join("").split("");
-    // console.log("join" + duplicateNumbers.join(""))
-    // console.log("numeros convertidos en array " + duplicateNumbers)
+    console.log("join" + duplicateNumbers.join(""))
+   
     finalArry = duplicateNumbers.concat(individualNumbers);
-    // console.log("array para sumar" + finalArry)
-    for (var j = 0; j < finalArry.length; j++) {
+    console.log("array para sumar" + finalArry)
+    for (let j = 0; j <= finalArry.length-1; j++) {
         sum += parseInt(finalArry[j]);
         console.log(sum);
     }
-    
 
     if (sum % 10 === 0) {
         // validCard = true;
@@ -56,7 +52,7 @@ const validator = {
  
 },
 
-//Enmascaramiento del numero de la tarjeta de credito
+//Enmascaramiento del numero de la tarjeta de crédito
   maskify: function mask(cardNumber) {
     let newNumber = cardNumber.replace(/\d(?=\d{4})/g, "#");
     return newNumber;
